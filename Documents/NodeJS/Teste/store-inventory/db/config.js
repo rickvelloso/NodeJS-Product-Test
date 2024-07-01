74059a2f99482e5bc64cbfd6188bc5efc53df315
PEDRO HENRIQUE BARRETO VELLOSO <129546152+rickvelloso@users.noreply.github.com>
2024-07-01 17:08:26 -0300

    Update config.js

--- Documents/NodeJS/Teste/store-inventory/db/config.js
+++ Documents/NodeJS/Teste/store-inventory/db/config.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', '180705', {
-  host: 'localhost',
+const sequelize = new Sequelize('***', '***', '***', {
+  host: '***',
   dialect: 'postgres',
 });
 
